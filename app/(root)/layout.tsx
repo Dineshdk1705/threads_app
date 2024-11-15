0ae740f0b1ad70fae8ee225610c90beb2c09bfd9
Dineshdk1705 <dnsh555g@gmail.com>
2024-11-15 18:06:28 +0530

    fix deployee render issue

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,18 +2,23 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import dynamic from "next/dynamic";
 
-import Header from "@/components/shared/Header";
-import LeftSideBar from "@/components/shared/LeftSideBar";
-import RightSideBar from "@/components/shared/RightSideBar";
-import Footer from "@/components/shared/Footer";
+const Header = dynamic(() => import("@/components/shared/Header"));
+const LeftSideBar = dynamic(() => import("@/components/shared/LeftSideBar"), {
+  ssr: false,
+});
+const RightSideBar = dynamic(() => import("@/components/shared/RightSideBar"), {
+  ssr: false,
+});
+const Footer = dynamic(() => import("@/components/shared/Footer"));
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Threads",
   description: "A Next.js 15 Meta Threads application",
 };
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -22,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
-    // publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <html lang="en">
         <body className={`${inter.className}`}>
@@ -30,7 +35,9 @@ export default function RootLayout({
           <main className="flex flex-row">
             <LeftSideBar />
             <section className="main-container">
-              <div className="w-full max-w-4xl">{children}</div>
+              <div className="children-container">
+                {children || <p>No content</p>}
+              </div>
             </section>
             <RightSideBar />
           </main>
